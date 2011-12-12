17bbd9264de94d0ff7e02b86e902f6cc631a6f9a
Eddie Gomez <eddie@talho.org>
2011-12-12 16:30:24 -0600

    Changed create method in users_controller.rb to always target Admins in talho with texas jurisdiction, changed Member to Staff.

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -3,7 +3,7 @@ r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_applicat
 p "#{r.name} - #{r.application}"
 r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Admin', true, false, 'talho')
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Member', true, true, 'talho')
+r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Staff', true, true, 'talho')
 p "#{r.name} - #{r.application}"
 r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_application('Director', true, true, 'talho')
 p "#{r.name} - #{r.application}"
