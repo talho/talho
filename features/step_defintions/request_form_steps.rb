ef25c1c81ef8f86e2c36078976580e48d630d9b6
cdubose <cdubose@marlin.(none)>
2012-03-20 10:58:23 -0500

    rake cucumber:talho should work now

--- features/step_defintions/request_form_steps.rb
+++ features/step_defintions/request_form_steps.rb
@@ -1,9 +1,9 @@
 Then /^a talho conference request from "([^"]*)" should be sent containing:$/ do |email_from, expected|
   sent = ActionMailer::Base.deliveries.first
-  assert_equal [DO_NOT_REPLY],                    sent.from
-  assert_equal [email_from],                      sent.bcc
+  assert_equal [DO_NOT_REPLY],                     sent.from
+  assert_equal [email_from],                       sent.bcc
   assert_equal [FormController::CONFERENCE_EMAIL], sent.to
-  assert_match /#{h expected.raw[0].first}/,      sent.subject
+  assert_match /#{h expected.raw[0].first}/,       sent.subject
   expected.raw.each do |row|
     assert_match /#{row[0]}/, sent.body
   end
@@ -11,10 +11,10 @@ end
 
 Then /^a talho helpdesk request from "([^"]*)" should be sent containing:$/ do |email_from, expected|
   sent = ActionMailer::Base.deliveries.first
-  assert_equal [DO_NOT_REPLY],                  sent.from
-  assert_equal [email_from],                    sent.bcc
+  assert_equal [DO_NOT_REPLY],                   sent.from
+  assert_equal [email_from],                     sent.bcc
   assert_equal [FormController::HELPDESK_EMAIL], sent.to
-  assert_match /#{h expected.raw[0].first}/,    sent.subject
+  assert_match /#{h expected.raw[0].first}/,     sent.subject
   expected.raw.each do |row|
     assert_match /#{row[0]}/, sent.body
   end
