64b63fc8b01cca00d67db0777b72f987d075b6b9
cdubose <cdubose@marlin.(none)>
2012-07-12 16:21:57 -0500

    Prepping talho for the role manager

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -1,13 +1,13 @@
 app = App.find_by_name('talho')
 
 p 'Writing Roles'
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Dashboard', false, false, app.id) do |role|
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Dashboard', false, app.id) do |role|
   role.public = true
 end
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Admin', true, false, app.id)
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Admin', false, app.id)
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Staff', true, true, app.id)
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Staff', true, app.id)
 p "#{r.name} - #{r.application}"
-r = Role.find_or_create_by_name_and_approval_required_and_user_role_and_app_id('Director', true, true, app.id)
+r = Role.find_or_create_by_name_and_user_role_and_app_id('Director', true, app.id)
 p "#{r.name} - #{r.application}"
